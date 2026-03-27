ff327c441a052fd259d8afb6812c1a5c48745dbd
henryvn2004-arch <henryvn2004@gmail.com>
2026-03-27 22:04:33 +0700

    Add files via upload

--- api/cron-khao-luan.js
+++ api/cron-khao-luan.js
@@ -134,7 +134,7 @@ async function insertArticle(table, article) {
   });
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.headers['authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
