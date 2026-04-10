86adf5dcdd20781b592abf2b5fc7f3a15b5b7fae
henryvn2004-arch <henryvn2004@gmail.com>
2026-04-10 23:57:43 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: false,
   async rewrites() {
     return [
       { source: '/',                    destination: '/index.html'           },
