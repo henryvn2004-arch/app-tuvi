7a2ec93c44c009d768474d0d024663151b9445d7
henryvn2004-arch <henryvn2004@gmail.com>
2026-04-12 09:11:12 +0000

    chore: remove patch scripts

--- debug-year.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('public/index.html', 'utf8');
-const idx = html.indexOf('Vận hạn năm');
-if (idx === -1) { console.log('Không thấy'); process.exit(); }
-console.log(JSON.stringify(html.slice(idx - 10, idx + 120)));
